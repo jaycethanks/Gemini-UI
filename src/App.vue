3116a4cb56310b94e662a87b73e9887af9b9898f
jayce <jaycethanks@outlook.com>
2024-01-16 16:13:10 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <div class="wrapper text-red-700 text-2xl">
+      hello
+    </div>
+  </header>
+
+  <main>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
